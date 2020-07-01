31c298679d26792eaa4df8dc2cbe26147b8ef483
2019315040_LeeJieun <aegkrc907@naver.com>
2020-07-01 13:45:16 +0900

    [random-play-01] Set a random play to the computer

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,8 +1,10 @@
+from random import randint
+
 #list of play options
 play = ["Rock", "Paper", "Scissors"]
 
 #assign a random play to the computer
-computer = "Rock"
+computer = play[randint(0,2)]
 print('Computer: {}'.format(computer))
 
 #get the user input
